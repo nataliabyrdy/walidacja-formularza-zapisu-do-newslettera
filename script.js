1e2f6c55bcd3b3b3e4e47e5c2500089102771bb9
nataliabyrdy <nataliabyrdy@onet.pl>
2023-10-02 22:11:51 +0200

    implementacja skryptu js

---
+++ script.js
@@ -0,0 +1,56 @@
+console.log("walidacja formularza");
+
+let newsletterForm = document.getElementById('name');
+let allAgreeChx = document.getElementById('all-agree');
+
+const validate = (event) => {
+
+    let txtName = document.getElementById('name');
+    let txtEmail = document.getElementById('email');
+    let agree1 = document.getElementById('agree-1');
+    let errors = document.getElementById('errors');
+
+    errors.innerHTML = '';
+
+    if (txtName.ariaValueMax.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = "Wypisz Imie i nazwisko";
+        errors.appendChild(liError);
+    }
+
+    if (txtEmail.value.trim() === '') {
+        let liError = document.createElement('li');
+        liError.innerText = 'Wpisz Adres e-mail!';
+        errors.appendChild(liError);
+    }
+
+    if (!txtEmail.value.includes('@')) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Adres e-mail musi zawierać @';
+        errors.appendChild(liError);
+    }
+
+    if (!agree1.checked) {
+        let liError = document.createElement('li');
+        liError.innerText = 'Nie wyraziłeś Zgody 1!';
+        errors.appendChild(liError);
+    }
+
+    if (errors.children.length > 0) {
+        event.preventDefault();
+    }
+}
+
+const allAgree = (event) => {
+    let agree1 = document.getElementById('agree-1');
+    let agree2 = document.getElementById('agree-2');
+
+    agree1.checked = event.target.checked;
+    agree2.checked = event.target.checked;
+
+    agree1.disabled = event.target.checked;
+    agree2.disabled = event.target.checked;
+}
+
+newsletterForm.addEventListener('submit', validate);
+allAgreeChx.addEventListener('change', allAgree);
